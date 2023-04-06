62df0fe47f4fee268807bc5af728e79000441617
Akshay Vayak <109885077+VayakAkshay@users.noreply.github.com>
2023-04-06 15:07:06 +0530

    all changes

--- Mainpage/admin.py
+++ Mainpage/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import ProductData,CartData,ContactForm,OrderItem
-# Register your models here.
 
-admin.site.register(ProductData)
-admin.site.register(CartData)
-admin.site.register(ContactForm)
-admin.site.register(OrderItem)
+class ProductDataAdmin(admin.ModelAdmin):
+    list_display = ["product_name","product_price","product_desc","product_cat"]
+
+class CartDataAdmin(admin.ModelAdmin):
+    list_display = ["product_id","qty","user_id"]
+
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ["email","message"]
+
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ["product_id","qty","user_id"]
+
+admin.site.register(ProductData,ProductDataAdmin)
+admin.site.register(CartData,CartDataAdmin)
+admin.site.register(ContactForm,ContactFormAdmin)
+admin.site.register(OrderItem,OrderItemsAdmin)
